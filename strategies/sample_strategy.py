a91465e0f39223d20a324db217fb9f8acdd40d01
michaelchu <mchchu88@gmail.com>
2018-08-21 06:06:58 +0800

    WIP - Filters

--- strategies/sample_strategy.py
+++ strategies/sample_strategy.py
@@ -1,41 +1,25 @@
 import optopsy as op
 
-# Filters take a single value or a tuple as parameters,
-# if a tuple is provided for a parameter, the simulation
-# will run every possible combinations across all filters.
-filters = {
-    'strategy': 'long_call_spread',
-    'start_date': '2016-12-01',
-    'end_date': '2016-12-31',
-    
-    'entry_leg1_abs_delta': 0.2,
-    'entry_leg1_abs_delta_min': 0.15,
-    'entry_leg1_abs_delta_max': 0.25,
-    
-    'entry_leg2_abs_delta': 0.4,
-    'entry_leg2_abs_delta_min': 0.35,
-    'entry_leg2_abs_delta_max': 0.45,
-    
-    'entry_dte': 47,
-    'entry_dte_min': 40,
-    'entry_dte_max': 52,
-    
-    'exit_dte': 1
-}
-
 
 def run_strategy():
     # fetch the option chains from our data source
-    data = op.data.get(
+    data = op.get(
         'data/SPX.csv',
         struct=op.structs.cboe_struct,
         prompt=False
     )
+    
+    entry_filters = {dte: op.Period.SEVEN_WEEKS, price: 1.0}
+    exit_filters = {}
 
     # test our strategy with our defined filter rules,
     # simulate function will return a dict with three dataframe items: summary, returns
     # and trades
-    backtest = op.backtest.simulate('Weekly Verticals', data, filters)
+    backtest = op.simulate('Weekly Verticals', 
+						   "long_call_spread",
+						   entry_filters=entry_filters,
+						   exit_filters=exit_filters
+					      )
 
 
 if __name__ == '__main__':
